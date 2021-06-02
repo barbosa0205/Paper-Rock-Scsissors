7113c33312ccb75ed9a2af37699e50643c04028a
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-06-02 09:54:15 -0500

    Adding day/night mode

--- src/index.js
+++ src/index.js
@@ -1,20 +1,38 @@
 const nightBtn = document.querySelector('#nightbtn')
-console.log(nightBtn)
+const colorMode = document.querySelector('#colormode')
+const header = document.querySelector('#header')
+const main = document.querySelector('#main')
 let nightMode = false
+let count = 0
 
 const changeNightMode = () => {
-  const colorMode = document.querySelector('#colormode')
   colorMode.className += ' moon-night'
   colorMode.classList.remove('moon-day')
+  colorMode.classList.remove('fa-moon')
+  colorMode.className += ' fa-sun'
   nightBtn.className += ' span-night'
   nightBtn.classList.remove('span-day')
+  header.className += ' header-night'
+  header.classList.remove('header-day')
+  main.className += ' main-night'
+  main.classList.remove('main-day')
   nightMode = true
   console.log(nightMode)
   return nightMode
 }
 
 const changeDayMode = () => {
-  console.log('modo dia activado')
+  colorMode.className += ' moon-day'
+  colorMode.classList.remove('moon-night')
+  colorMode.classList.remove('fa-sun')
+  colorMode.className += ' fa-moon'
+  nightBtn.className += ' span-day'
+  nightBtn.classList.remove('span-night')
+  header.className += ' header-day'
+  header.classList.remove('header-night')
+  main.className += ' main-day'
+  main.classList.remove('main-night')
+
   nightMode = false
   console.log(nightMode)
   return nightMode
