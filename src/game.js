73a6c9edbd75f6bb2742c2b01df7abfe177a9c18
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-06-03 04:28:55 -0500

    Ending the logical part of the game

--- src/game.js
+++ src/game.js
@@ -3,8 +3,35 @@ const btnPaper = document.querySelector('#btn-paper')
 const btnScissor = document.querySelector('#btn-scissor')
 const btnPlay = document.querySelector('#btn-play')
 const option = document.querySelector('#option')
+const CPU = document.querySelector('#cpu')
+const random = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-const toPlay = () => {}
+const toPlay = op => {
+  cpu = random(1, 3)
+
+  console.log(cpu, CPU)
+
+  if (cpu == 1) {
+    CPU.src = './src/media/icons/rock.png'
+  } else if (cpu == 2) {
+    CPU.src = './src/media/icons/paper.png'
+  } else {
+    CPU.src = './src/media/icons/scissor.png'
+  }
+  if ((op == 1 && cpu == 3) || (op == 2 && cpu == 1) || (op == 3 && cpu == 2)) {
+    console.log('Yow Win')
+  } else if (
+    (op == 1 && cpu == 1) ||
+    (op == 2 && cpu == 2) ||
+    (op == 3 && cpu == 3)
+  ) {
+    console.log('Tie')
+  } else {
+    console.log('Cpu Win')
+  }
+}
 
 let op = 1
 btnRock.addEventListener('click', () => {
