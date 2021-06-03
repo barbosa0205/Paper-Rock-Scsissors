910b458af420f42a16dc34c6e0f01f18b518f088
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-06-03 17:09:36 -0500

    Adding buttons to retry and reset, and fix a bug in the alert

--- src/game.js
+++ src/game.js
@@ -7,6 +7,19 @@ const CPU = document.querySelector('#cpu')
 const overlay = document.querySelector('#overlay')
 const alert = document.querySelector('#alert')
 const message = document.querySelector('#message')
+const btnRetry = document.querySelector('#btn-retry')
+const btnReset = document.querySelector('#btn-reset')
+let e
+
+//Close Alert
+const closeAlert = () => {
+  overlay.classList.remove('overlay-visible')
+  alert.classList.remove('alert-visible')
+  alert.classList.remove('alert-won')
+  alert.classList.remove('alert-tie')
+  alert.classList.remove('alert-lost')
+  CPU.src = './src/media/icons/interrogationSign.png'
+}
 
 const animation = e => {
   if (e == 1) {
@@ -32,10 +45,8 @@ const random = (min, max) => {
 }
 
 const toPlay = op => {
-  let e = 0
   cpu = random(1, 3)
-
-  console.log(cpu, CPU)
+  console.log(cpu)
 
   if (cpu == 1) {
     CPU.src = './src/media/icons/rock.png'
@@ -44,39 +55,52 @@ const toPlay = op => {
   } else {
     CPU.src = './src/media/icons/scissor.png'
   }
+
   if ((op == 1 && cpu == 3) || (op == 2 && cpu == 1) || (op == 3 && cpu == 2)) {
-    animation((e = 1))
+    e = 1
+    animation(e)
+    console.log(e)
   } else if (
     (op == 1 && cpu == 1) ||
     (op == 2 && cpu == 2) ||
     (op == 3 && cpu == 3)
   ) {
-    animation((e = 2))
+    e = 2
+    animation(e)
+    console.log(e)
   } else {
-    animation((e = 3))
+    e = 3
+    animation(e)
+    console.log(e)
   }
 }
 
+//dynamic change in the option img
 let op = 1
 btnRock.addEventListener('click', () => {
   op = 1
-  console.log(option)
   option.src = './src/media/icons/rock.png'
   return option
 })
 btnPaper.addEventListener('click', () => {
   op = 2
-  console.log(option)
   option.src = './src/media/icons/paper.png'
   return option
 })
 btnScissor.addEventListener('click', () => {
   op = 3
-  console.log(option)
   option.src = './src/media/icons/scissor.png'
   return option
 })
 
 btnPlay.addEventListener('click', () => {
   toPlay(op)
+})
+
+btnRetry.addEventListener('click', () => {
+  closeAlert()
+})
+
+btnReset.addEventListener('click', () => {
+  closeAlert()
 })
