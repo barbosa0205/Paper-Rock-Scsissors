c428ebe09933c34f33521b8cc05588cac64c60d2
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-06-05 06:03:59 -0500

    we add option interactive change in customizing area

--- src/customize.js
+++ src/customize.js
@@ -2,7 +2,7 @@ const closePainter = document.querySelector('#close-painter')
 const overlayPainter = document.querySelector('.overlay-painter')
 const selectHand = document.querySelector('#select-hand')
 const applyHand = document.querySelector('#apply-hand')
-const imgPainter = document.querySelector('#img-painter')
+const imgPainter = document.querySelector('.img-painter')
 
 const openPaint = () => {
   overlayPainter.className += ' overlay-painter-visible'
@@ -13,22 +13,25 @@ const closePaint = () => {
 
 const changeColorHand = op => {
   if (op.value == 'white') {
-    console.log('Hola, cambie de color', option)
     rps.rock = './src/media/icons/rock.png'
     rps.paper = './src/media/icons/paper.png'
     rps.scissor = './src/media/icons/scissor.png'
+    imgPainter.src = rps.scissor
   } else if (op.value == 'yellow') {
     rps.rock = './src/media/icons/rock-yellow.png'
     rps.paper = './src/media/icons/paper-yellow.png'
     rps.scissor = './src/media/icons/scissor-yellow.png'
+    imgPainter.src = rps.scissor
   } else if (op.value == 'brown') {
     rps.rock = './src/media/icons/rock-brown.png'
     rps.paper = './src/media/icons/paper-brown.png'
     rps.scissor = './src/media/icons/scissor-brown.png'
+    imgPainter.src = rps.scissor
   } else if (op.value == 'orange') {
     rps.rock = './src/media/icons/rock-orange.png'
     rps.paper = './src/media/icons/paper-orange.png'
     rps.scissor = './src/media/icons/scissor-orange.png'
+    imgPainter.src = rps.scissor
   }
 }
 
