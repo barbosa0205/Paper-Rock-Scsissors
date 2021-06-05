8ef33147b06ce59b75fa8a45875376cc5129ac8a
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-06-05 03:37:47 -0500

    Adding painting custumer overlay

--- src/customize.js
+++ src/customize.js
@@ -1,1 +1,20 @@
+const close = document.querySelector('#close')
+const overlayPainter = document.querySelector('.overlay-painter')
 
+const openPaint = () => {
+  overlayPainter.className += ' overlay-painter-visible'
+}
+
+const closePaint = () => {
+  overlayPainter.classList.remove('overlay-painter-visible')
+}
+
+btnRoller.addEventListener('click', () => {
+  openPaint()
+})
+
+btnSetting.addEventListener('click', () => {})
+
+close.addEventListener('click', () => {
+  closePaint()
+})
