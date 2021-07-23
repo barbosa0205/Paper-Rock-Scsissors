75087b38cb4437ad78166c2ac1ab5ff5743773c0
Jose Luis Barbosa Cepeda <wichopianist@gmail.com>
2021-07-23 02:09:17 -0500

    Fixing customize container

--- src/customize.js
+++ src/customize.js
@@ -1,98 +1,82 @@
-const closePainter = document.querySelector('#close-painter')
-const overlayPainter = document.querySelector('.overlay-painter')
-const selectHand = document.querySelector('#select-hand')
-selectButton = document.querySelector('#select-button')
-const applyHand = document.querySelector('#apply-hand')
-const applyButton = document.querySelector('#apply-button')
-const imgPainter = document.querySelector('.img-painter')
+const closePainter = document.querySelector("#close-painter");
+const overlayPainter = document.querySelector(".overlay-painter");
+const selectHand = document.querySelector("#select-hand");
+const applyHand = document.querySelector("#apply-hand");
+const imgPainter = document.querySelector(".img-painter");
 
 const openPaint = () => {
-  overlayPainter.className += ' overlay-painter-visible'
-}
+  overlayPainter.className += " overlay-painter-visible";
+};
 const closePaint = () => {
-  overlayPainter.classList.remove('overlay-painter-visible')
-}
+  overlayPainter.classList.remove("overlay-painter-visible");
+};
 
-const changeColorHand = op => {
-  if (op.value == 'white') {
-    rps.rock = './src/media/icons/rock.png'
-    rps.paper = './src/media/icons/paper.png'
-    rps.scissor = './src/media/icons/scissor.png'
+const changeColorHand = (op) => {
+  if (op.value == "white") {
+    rps.rock = "./src/media/icons/rock.png";
+    rps.paper = "./src/media/icons/paper.png";
+    rps.scissor = "./src/media/icons/scissor.png";
     if (optionPlayer == 1) {
-      option.src = './src/media/icons/rock.png'
+      option.src = "./src/media/icons/rock.png";
     } else if (optionPlayer == 2) {
-      option.src = './src/media/icons/paper.png'
+      option.src = "./src/media/icons/paper.png";
     } else {
-      option.src = './src/media/icons/scissor.png'
+      option.src = "./src/media/icons/scissor.png";
     }
-    imgPainter.src = rps.scissor
-  } else if (op.value == 'yellow') {
-    rps.rock = './src/media/icons/rock-yellow.png'
-    rps.paper = './src/media/icons/paper-yellow.png'
-    rps.scissor = './src/media/icons/scissor-yellow.png'
+    imgPainter.src = rps.scissor;
+  } else if (op.value == "yellow") {
+    rps.rock = "./src/media/icons/rock-yellow.png";
+    rps.paper = "./src/media/icons/paper-yellow.png";
+    rps.scissor = "./src/media/icons/scissor-yellow.png";
     if (optionPlayer == 1) {
-      option.src = './src/media/icons/rock-yellow.png'
+      option.src = "./src/media/icons/rock-yellow.png";
     } else if (optionPlayer == 2) {
-      option.src = './src/media/icons/paper-yellow.png'
+      option.src = "./src/media/icons/paper-yellow.png";
     } else {
-      option.src = './src/media/icons/scissor-yellow.png'
+      option.src = "./src/media/icons/scissor-yellow.png";
     }
-    imgPainter.src = rps.scissor
-  } else if (op.value == 'brown') {
-    rps.rock = './src/media/icons/rock-brown.png'
-    rps.paper = './src/media/icons/paper-brown.png'
-    rps.scissor = './src/media/icons/scissor-brown.png'
+    imgPainter.src = rps.scissor;
+  } else if (op.value == "brown") {
+    rps.rock = "./src/media/icons/rock-brown.png";
+    rps.paper = "./src/media/icons/paper-brown.png";
+    rps.scissor = "./src/media/icons/scissor-brown.png";
     if (optionPlayer == 1) {
-      option.src = './src/media/icons/rock-brown.png'
+      option.src = "./src/media/icons/rock-brown.png";
     } else if (optionPlayer == 2) {
-      option.src = './src/media/icons/paper-brown.png'
+      option.src = "./src/media/icons/paper-brown.png";
     } else {
-      option.src = './src/media/icons/scissor-brown.png'
+      option.src = "./src/media/icons/scissor-brown.png";
     }
-    imgPainter.src = rps.scissor
-  } else if (op.value == 'orange') {
-    rps.rock = './src/media/icons/rock-orange.png'
-    rps.paper = './src/media/icons/paper-orange.png'
-    rps.scissor = './src/media/icons/scissor-orange.png'
+    imgPainter.src = rps.scissor;
+  } else if (op.value == "orange") {
+    rps.rock = "./src/media/icons/rock-orange.png";
+    rps.paper = "./src/media/icons/paper-orange.png";
+    rps.scissor = "./src/media/icons/scissor-orange.png";
     if (optionPlayer == 1) {
-      option.src = './src/media/icons/rock-orange.png'
+      option.src = "./src/media/icons/rock-orange.png";
     } else if (optionPlayer == 2) {
-      option.src = './src/media/icons/paper-orange.png'
+      option.src = "./src/media/icons/paper-orange.png";
     } else {
-      option.src = './src/media/icons/scissor-orange.png'
+      option.src = "./src/media/icons/scissor-orange.png";
     }
-    imgPainter.src = rps.scissor
+    imgPainter.src = rps.scissor;
   }
-}
+};
 
-const changeColorButton = op => {
-  if (op.value == 'red') {
-  } else if (op.value == 'purple') {
-  } else if (op.value == 'green') {
-  } else if (op.value == 'yellow') {
+const setColor = (op) => {
+  if (op.id == "select-hand") {
+    changeColorHand(op);
   }
-}
+};
 
-const setColor = op => {
-  if (op.id == 'select-hand') {
-    changeColorHand(op)
-  } else if (op.id == 'select-button') {
-    changeColorButton(op)
-  }
-}
+btnRoller.addEventListener("click", () => {
+  openPaint();
+});
 
-btnRoller.addEventListener('click', () => {
-  openPaint()
-})
+closePainter.addEventListener("click", () => {
+  closePaint();
+});
 
-closePainter.addEventListener('click', () => {
-  closePaint()
-})
-
-applyHand.addEventListener('click', () => {
-  setColor(selectHand)
-})
-
-applyButton.addEventListener('click', () => {
-  setColor(selectButton)
-})
+applyHand.addEventListener("click", () => {
+  setColor(selectHand);
+});
